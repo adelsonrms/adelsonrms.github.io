163c564622d6a062f67746a9dd5b3a31cdb4e05e
Adelson Rosendo Marques da Silva <adelson@tecnun.com.br>
2022-03-11 16:35:26 -0300

    Atualizado index e incluindo timesheet.js

--- timesheet.js
+++ timesheet.js
@@ -1,76 +1,54 @@
 (function () {
+
     // Inicializa o objeto do Conector do Tableau
     var myConnector = tableau.makeConnector();
 
     // Define o esquema da tabelas que serão carregadas pela api
     myConnector.getSchema = function (schemaCallback) {
 
-        //Schema do objeto de usuarios
-        /*
-        
-        {
-                    "id_usuario": 1,
-                    "nm_nome": "VITOR LUIZ RUBIO",
-                    "ds_email": "vitor@tecnun.com.br",
-                    "ds_cargo": "ANALISTA PROGRAMADOR SÊNIOR",
-                    "tp_tipo": 1,
-                    "fl_ativo": false
+        var tb_schema_usuario =  {
+            id: "tb_usuarios",
+            alias: "Usuario",
+            columns: [
+                {"id": "id_usuario", "dataType": "int"},
+                {"id": "nm_nome","dataType": "string"},
+                {
+                    "id": "ds_email",
+                    "dataType": "string"
+                },
+                {
+                    "id": "ds_cargo",
+                    "dataType": "string"
+                },
+                {
+                    "id": "tp_tipo",
+                    "dataType": "int"
+                },
+                {
+                    "id": "fl_ativo",
+                    "dataType": "bool"
                 }
-        
-        */
-        var cols = [
-            {
-                "id": "id_usuario",
-                "alias": "id",
-                "dataType": "float", "descrption": "ID unico do usuario"
-            },
-            {
-                "id": "nm_nome",
-                "alias": "Nome",
-                "dataType": "string"
-            },
-            {
-                "id": "ds_email",
-                "alias": "Email",
-                "dataType": "string"
-            },
-            {
-                "id": "ds_cargo",
-                "alias": "Cargo",
-                "dataType": "string"
-            },
-            {
-                "id": "tp_tipo",
-                "alias": "Tipo",
-                "dataType": "float"
-            },
-            {
-                "id": "fl_ativo",
-                "dataType": "bool"
-            }
-        ];
-
-        var tableSchema = {
-            id: "usuarios",
-            alias: "Tabela de usuarios cadastrados",
-            columns: cols
+            ]
         };
 
-        schemaCallback([tableSchema]);
+        schemaCallback([tb_schema_usuario]);
     };
 
     // Função que será executada ao clicar no botão de buscar os dados. 
     // Aqui será carregado as informações da url da API
     myConnector.getData = function (table, doneCallback) {
 
-        //Se conecta a API e recupera os dados desejados
-        $.getJSON("https://apitecnun.azurewebsites.net/v2/rh/usuarios?ClientID=app_pbi_pedro&ClientSecret=123456&cliente=3886&X-API-Key=CD9190C7495A4D1F83CF95E198BF2515", function (resp) {
+        //Tabela de saida
+        var tableData = [];
+
+        console.log('Inicia a carga de Usuarios....')
+        //USUARIOS - Se conecta a API e recupera os dados desejados
+        $.getJSON("https://apitecnun.azurewebsites.net/v2/rh/usuarios?ClientID=app_pbi_pedro&ClientSecret=123456", function (resp) {
 
             //Os dados da lista no objeto de retorno Result.Data
             var dados = resp.data;
 
-            //Tabela de saida
-            var tableData = [];
+            console.log('Usuarios - ' + resp.message)
 
             // Iterate over the JSON object
             for (var i = 0, len = dados.length; i < len; i++) {
@@ -85,20 +63,133 @@
                 });
             }
 
+            console.log('Terminou de carrega a tabela de Usuarios. Adiciona na tabela (appendRows)')
             //Adiciona os dados recuperados para o tableau
             table.appendRows(tableData);
-            
+
             //Função de callback. Será processada pelo Tableau
             doneCallback();
+
+            // console.log('Inicia a carga de Clientes....')
+
+            // //Prepara o objero para um novo schema
+            // tableData = [];
+
+            // //CLIENTES 
+            // $.getJSON("https://apitecnun.azurewebsites.net/v2/comercial/clientes?ClientID=app_pbi_pedro&ClientSecret=123456", function (resp) {
+
+            //     //Os dados da lista no objeto de retorno Result.Data
+            //     var dados = resp.data;
+            //     console.log('Clientes - ' + resp.message)
+
+            //     // Iterate over the JSON object
+            //     for (var i = 0, len = dados.length; i < len; i++) {
+
+            //         tableData.push({
+            //             "id_cliente": dados[i].id_cliente,
+            //             "nm_nome": dados[i].nm_nome,
+            //             "ds_razaoSocial": dados[i].ds_razaoSocial,
+            //             "nr_cnpj": dados[i].nr_cnpj,
+            //             "ds_contato": dados[i].ds_contato
+            //         });
+            //     }
+
+            //     console.log('Terminou de carrega a tabela de Clientes. Adiciona na tabela (appendRows)')
+            //     //Adiciona os dados recuperados para o tableau
+            //     table.appendRows(tableData);
+
+            // });
         });
+
+
     };
 
     tableau.registerConnector(myConnector);
 
+    // Inicializa o objeto do Conector do Tableau
+    var myConnectorCliente = tableau.makeConnector();
+
+    // Define o esquema da tabelas que serão carregadas pela api
+    myConnectorCliente.getSchema = function (schemaCallback) {
+        var tb_schema_cliente = {
+            id: "tb_cliente",
+            alias: "Cliente",
+            columns: [
+                {
+                    "id": "id_cliente",
+                    "dataType": "int"
+                },
+                {
+                    "id": "nm_nome",
+                    "dataType": "string"
+                },
+                {
+                    "id": "ds_razaoSocial",
+                    "dataType": "string"
+                },
+                {
+                    "id": "nr_cnpj",
+                    "dataType": "string"
+                },
+                {
+                    "id": "ds_contato",
+                    "dataType": "string"
+                }
+            ]
+        };
+
+        schemaCallback([tb_schema_cliente]);
+    };
+
+    // Função que será executada ao clicar no botão de buscar os dados. 
+    // Aqui será carregado as informações da url da API
+    myConnectorCliente.getData = function (table, doneCallback) {
+
+        //Tabela de saida
+        var tableData = [];
+
+        console.log('Inicia a carga de Usuarios....')
+
+          //CLIENTES 
+            $.getJSON("https://apitecnun.azurewebsites.net/v2/comercial/clientes?ClientID=app_pbi_pedro&ClientSecret=123456", function (resp) {
+
+                //Os dados da lista no objeto de retorno Result.Data
+                var dados = resp.data;
+                console.log('Clientes - ' + resp.message)
+
+                // Iterate over the JSON object
+                for (var i = 0, len = dados.length; i < len; i++) {
+
+                    tableData.push({
+                        "id_cliente": dados[i].id_cliente,
+                        "nm_nome": dados[i].nm_nome,
+                        "ds_razaoSocial": dados[i].ds_razaoSocial,
+                        "nr_cnpj": dados[i].nr_cnpj,
+                        "ds_contato": dados[i].ds_contato
+                    });
+                }
+
+                console.log('Terminou de carrega a tabela de Clientes. Adiciona na tabela (appendRows)')
+                //Adiciona os dados recuperados para o tableau
+                table.appendRows(tableData);
+
+                doneCallback();
+            });
+    };
+
+    tableau.registerConnector(myConnectorCliente);
+
     // Create event listeners for when the user submits the form
     $(document).ready(function () {
+
         $("#submitButton").click(function () {
-            tableau.connectionName = "Tecnun Api Rest - Clientes"; // This will be the data source name in Tableau
+            tableau.connectionName = "Tecnun Api DataSource - Usuarios"; // This will be the data source name in Tableau
+            tableau.submit(); // This sends the connector object to Tableau
+        });
+
+
+        $("#btn-carregar-clientes").click(function () {
+            tableau.connectionName = "Tecnun Api DataSource - Clientes"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
         });
     });
